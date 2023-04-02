b657aeedf436b0e4de3f28a16afa6aebce409692
czarfbc <cezarfbc@gmail.com>
2023-04-02 00:38:43 -0300

    semi-terminado

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const next = document.querySelector('.btn_next');
 
 var searchPokemon = 1;
 
-
 const FetchPokemon = async (pokemon) => {
     try {
         const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
@@ -29,39 +28,106 @@ const InfoPokemon = async (pokemon) => {
         idPokemon.innerHTML = '';
 
         const dataAPI = await FetchPokemon(pokemon);
-
+        
         const genus = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`);
         const dataGenus = await genus.json()
         const genusNameEnglish = dataGenus.genera[7].genus;
         const nameGenus = genusNameEnglish.replace('Pokémon', '');
+        const textGenus = document.querySelector('.textGenus')
+        
+        
 
         const weaknesses = await fetch(dataAPI.types[0].type.url) 
         const dataWeaknesses = await weaknesses.json()
         const nameWeaknessesArray = dataWeaknesses.damage_relations.double_damage_from;
 
-        console.log(dataAPI['sprites']['versions']['generation-v']['black-white']['animated']['front_default']) 
         spritePokemon.src = dataAPI['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
         spritePokemon.style.display = 'block';
 
-        console.log(`id: ${dataAPI.id} name: ${dataAPI.name}`)
-        namePokemon.innerHTML = dataAPI.name;
+        namePokemon.innerHTML = dataAPI.name.toUpperCase();
         idPokemon.innerHTML = dataAPI.id;
 
-        console.log(`Genus: ${nameGenus}`);
 
-        console.log(`ability: ${dataAPI.abilities[0].ability.name}`)
+        textGenus.innerHTML = nameGenus
+
+        const textAbility = document.querySelector('.textAbility')
+        textAbility.innerHTML = dataAPI.abilities[0].ability.name
 
         dataAPI.types.forEach(element => {
-            console.log(`types: ${element.type.name}`)
+            const textTypes_1 = document.querySelector('.textTypes_1')
+            textTypes_1.innerHTML = dataAPI.types[0].type.name
+
+            const textTypes_2 = document.querySelector('.textTypes_2')
+            if(dataAPI.types.length > 1){
+                textTypes_2.innerHTML = dataAPI.types[1].type.name
+            }else{
+                textTypes_2.innerHTML = ''
+            }
+            
         });
+        
+
+        
 
         nameWeaknessesArray.forEach(element => {
-            console.log(`weaknesses: ${element.name}`)
+            
+            const textWeaknesses_1 = document.querySelector('.textWeaknesses_1')
+            const textWeaknesses_2 = document.querySelector('.textWeaknesses_2')
+            const textWeaknesses_3 = document.querySelector('.textWeaknesses_3')
+            const textWeaknesses_4 = document.querySelector('.textWeaknesses_4')
+            const textWeaknesses_5 = document.querySelector('.textWeaknesses_5')
+            
+            switch (nameWeaknessesArray.length) {
+                case 1:
+                    textWeaknesses_5.innerHTML =  ''
+                    textWeaknesses_4.innerHTML =  ''
+                    textWeaknesses_3.innerHTML =  ''
+                    textWeaknesses_2.innerHTML =  ''
+                    textWeaknesses_1.innerHTML =  nameWeaknessesArray[0].name
+                    break;
+            
+                case 2:
+                    textWeaknesses_5.innerHTML =  ''
+                    textWeaknesses_4.innerHTML =  ''
+                    textWeaknesses_3.innerHTML =  ''
+                    textWeaknesses_2.innerHTML =  nameWeaknessesArray[1].name
+                    textWeaknesses_1.innerHTML =  nameWeaknessesArray[0].name
+                    break;
+            
+                case 3:
+                    textWeaknesses_5.innerHTML =  ''
+                    textWeaknesses_4.innerHTML =  ''
+                    textWeaknesses_3.innerHTML =  nameWeaknessesArray[2].name
+                    textWeaknesses_2.innerHTML =  nameWeaknessesArray[1].name
+                    textWeaknesses_1.innerHTML =  nameWeaknessesArray[0].name
+                    break;
+            
+                case 4:
+                    textWeaknesses_5.innerHTML =  ''
+                    textWeaknesses_4.innerHTML =  nameWeaknessesArray[3].name
+                    textWeaknesses_3.innerHTML =  nameWeaknessesArray[2].name
+                    textWeaknesses_2.innerHTML =  nameWeaknessesArray[1].name
+                    textWeaknesses_1.innerHTML =  nameWeaknessesArray[0].name
+                    break;
+            
+                case 5:
+                    textWeaknesses_5.innerHTML =  nameWeaknessesArray[4].name
+                    textWeaknesses_4.innerHTML =  nameWeaknessesArray[3].name
+                    textWeaknesses_3.innerHTML =  nameWeaknessesArray[2].name
+                    textWeaknesses_2.innerHTML =  nameWeaknessesArray[1].name
+                    textWeaknesses_1.innerHTML =  nameWeaknessesArray[0].name
+                    break;
+            
+                default:
+                    break;
+            }
         });
+
         input.value = '';
         searchPokemon = dataAPI.id;
+        
     } catch (error) {
-        console.log(error)
+        console.error(error)
         spritePokemon.style.display = 'none';
         namePokemon.innerHTML = 'Not Found :(';
         idPokemon.innerHTML = '';
@@ -74,6 +140,7 @@ form.addEventListener('submit', (event)=>{
     InfoPokemon(input.value.toLowerCase());
 });
 submit.addEventListener('click', ()=>{
+    
     if(input.value != ""){
         InfoPokemon(input.value.toLowerCase());
     }
